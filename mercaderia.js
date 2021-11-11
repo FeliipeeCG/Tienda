85201124158887302adcf58598c4e58593d33ca6
FeliipeeCG <felipecerquin@hotmail.com>
2021-11-10 22:35:38 -0300

    main

--- mercaderia.js
+++ mercaderia.js
@@ -14,7 +14,7 @@ estante_figura.push(figura4);
 
 //Estante
 let estante = document.getElementById("estante");
-$(Document).ready(() => {
+$(document).ready(() => {
   for (let i = 0; i < estante_figura.length; i++) {
     let objeto_figura = estante_figura[i];
     $("#estante").append(
